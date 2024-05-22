1a70bf2be597da5d0fd9aa884a141c7df6208dc8
David Tache <tached@uninorte.edu.co>
2024-05-21 22:06:38 -0500

    visual help changes

--- src/components/Help/Help.jsx
+++ src/components/Help/Help.jsx
@@ -27,6 +27,36 @@ const Help = ({ onClose }) => {
           our recommendations and find your next vehicle with confidence!
           <br />
           <br />
+          <ul>
+            <li>
+              <strong>Year:</strong> 1990-20221
+            </li>
+            <li>
+              <strong>Kilometer:</strong> 0-100.000
+            </li>
+            <li>
+              <strong>Owner:</strong> 0 - first owner, 1 - 4th or above, 2 - 2nd
+              owner, 3 - 3rd owner
+            </li>
+            <li>
+              <strong>Transmission:</strong> 0 - automatic, 1 - manual
+            </li>
+            <li>
+              <strong>Fuel:</strong> 0 - GNC, 1 - Diesel, 2 - LPG, 3 - Petrol
+            </li>
+            <li>
+              <strong>Mileage:</strong> 15-40
+            </li>
+            <li>
+              <strong>Engine:</strong> 800-6000
+            </li>
+            <li>
+              <strong>Seats:</strong> 2-10
+            </li>
+            <li>
+              <strong>Price:</strong> 1000-300.000
+            </li>
+          </ul>
           <br />
           Created By:
           <br />
