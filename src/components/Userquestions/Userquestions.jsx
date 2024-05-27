3866414a9a64cd8e617b661e0f74359698a61152
David Tache <tached@uninorte.edu.co>
2024-05-27 12:25:27 -0500

    chaging input methods

--- src/components/Userquestions/Userquestions.jsx
+++ src/components/Userquestions/Userquestions.jsx
@@ -25,10 +25,42 @@ const UserQuestions = ({ check }) => {
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    // Save the user's responses to the localStorage
-    localStorage.setItem("userResponses", JSON.stringify(formValues));
+    // Validation function
+    const isValid = () => {
+      const {
+        Year,
+        Kilometer,
+        Fuel,
+        Transmission,
+        Owner,
+        Mileage,
+        Engine,
+        Seats,
+        Price,
+      } = formValues;
 
-    check();
+      if (!Year || Year < 1990 || Year > 2022) return false;
+      if (!Kilometer || Kilometer < 0 || Kilometer > 100000) return false;
+      if (Fuel === "none" || Fuel < 0 || Fuel > 3) return false;
+      if (Transmission === "none" || Transmission < 0 || Transmission > 1)
+        return false;
+      if (Owner === "none" || Owner < 0 || Owner > 3) return false;
+      if (!Mileage || Mileage < 15 || Mileage > 40) return false;
+      if (!Engine || Engine < 800 || Engine > 6000) return false;
+      if (!Seats || Seats < 2 || Seats > 10) return false;
+      if (!Price || Price < 1000 || Price > 300000) return false;
+
+      return true;
+    };
+
+    if (isValid()) {
+      // Save the user's responses to the localStorage
+      localStorage.setItem("userResponses", JSON.stringify(formValues));
+      
+      check();
+    } else {
+      alert("Please fill all the fields correctly...  Using older values.");
+    }
   };
 
   return (
@@ -41,6 +73,8 @@ const UserQuestions = ({ check }) => {
           value={formValues.Year}
           onChange={handleChange}
           placeholder="Year"
+          min="1990"
+          max="2022"
         />
         <input
           className={styles.inputbar}
@@ -49,31 +83,49 @@ const UserQuestions = ({ check }) => {
           value={formValues.Kilometer}
           onChange={handleChange}
           placeholder="Kilometer"
+          min="0"
+          max="100000"
         />
-        <input
+        <select
           className={styles.inputbar}
-          type="number"
           name="Fuel"
           value={formValues.Fuel}
           onChange={handleChange}
-          placeholder="Fuel"
-        />
-        <input
+        >
+          <option value="none" disabled defaultValue>
+            Fuel
+          </option>
+          <option value="0">GNC</option>
+          <option value="1">Diesel</option>
+          <option value="2">LPG</option>
+          <option value="3">Petrol</option>
+        </select>
+        <select
           className={styles.inputbar}
-          type="number"
           name="Transmission"
           value={formValues.Transmission}
           onChange={handleChange}
-          placeholder="Transmission"
-        />
-        <input
+        >
+          <option value="none" disabled defaultValue>
+            Transmission
+          </option>
+          <option value="0">Automatic</option>
+          <option value="1">Manual</option>
+        </select>
+        <select
           className={styles.inputbar}
-          type="number"
           name="Owner"
           value={formValues.Owner}
           onChange={handleChange}
-          placeholder="Owner"
-        />
+        >
+          <option value="none" disabled defaultValue>
+            Owner
+          </option>
+          <option value="0">First owner</option>
+          <option value="1">4th or above</option>
+          <option value="2">2nd owner</option>
+          <option value="3">3rd owner</option>
+        </select>
         <input
           className={styles.inputbar}
           type="number"
@@ -81,6 +133,8 @@ const UserQuestions = ({ check }) => {
           value={formValues.Mileage}
           onChange={handleChange}
           placeholder="Mileage"
+          min="15"
+          max="40"
         />
         <input
           className={styles.inputbar}
@@ -89,6 +143,8 @@ const UserQuestions = ({ check }) => {
           value={formValues.Engine}
           onChange={handleChange}
           placeholder="Engine"
+          min="800"
+          max="6000"
         />
         <input
           className={styles.inputbar}
@@ -97,6 +153,8 @@ const UserQuestions = ({ check }) => {
           value={formValues.Seats}
           onChange={handleChange}
           placeholder="Seats"
+          min="2"
+          max="10"
         />
         <input
           className={styles.inputbar}
@@ -105,8 +163,10 @@ const UserQuestions = ({ check }) => {
           value={formValues.Price}
           onChange={handleChange}
           placeholder="Price"
+          min="1000"
+          max="300000"
         />
-        <button className={styles.submitB} type="submit" onClick={check}>
+        <button className={styles.submitB} type="submit">
           Submit
         </button>
       </form>
